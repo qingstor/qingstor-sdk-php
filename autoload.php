bf412c430d544949269aa76a69e61478cea3ea74
Xuanwo <xuanwo@yunify.com>
2017-02-03 14:39:47 +0800

    template: Fix typo & remove extra space
    Signed-off-by: Xuanwo <xuanwo@yunify.com>

--- autoload.php
+++ autoload.php
@@ -1,8 +1,9 @@
 <?php
+
 function classLoader($class)
 {
     $path = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-    $file = __DIR__ . DIRECTORY_SEPARATOR .'src'. DIRECTORY_SEPARATOR . $path . '.php';
+    $file = __DIR__.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.$path.'.php';
     if (file_exists($file)) {
         require_once $file;
     }
