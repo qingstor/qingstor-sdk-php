db324969dba33f3e718055037399f0e699e37d73
Xuanwo <xuanwo@yunify.com>
2018-11-14 14:44:58 +0800

    Fix code style

--- tests/client/ImageProcessClientTest.php
+++ tests/client/ImageProcessClientTest.php
@@ -22,131 +22,131 @@ use PHPUnit\Framework\TestCase;
 
 class ImageProcessClientTest extends TestCase
 {
-	
     public function test_info()
     {
-		$client = new ImageProcessClient('', '');
-		$client->info();
-		$this->assertEquals(
-			'info',
-			$client->input['action']
-		);
+        $client = new ImageProcessClient('', '');
+        $client->info();
+        $this->assertEquals(
+            'info',
+            $client->input['action']
+        );
     }
 
-	public function test_crop()
-	{
-		$client = new ImageProcessClient('', '');
-		$data = array (
-			'gravity' => 0		
-		);
-		$client->crop($data);
-		$this->assertEquals(
-			'crop:w_0,h_0,g_0',
-			$client->input['action']
-		);
-	}
+    public function test_crop()
+    {
+        $client = new ImageProcessClient('', '');
+        $data = array(
+            'gravity' => 0
+        );
+        $client->crop($data);
+        $this->assertEquals(
+            'crop:w_0,h_0,g_0',
+            $client->input['action']
+        );
+    }
 
-	public function test_rotate()
-	{
-		$client = new ImageProcessClient('', '');
-		$data = array (
-			'angle' => 90
-		);
-		$client->rotate($data);
-		$this->assertEquals(
-			'rotate:a_90',
-			$client->input['action']
-		);
-	}
+    public function test_rotate()
+    {
+        $client = new ImageProcessClient('', '');
+        $data = array(
+            'angle' => 90
+        );
+        $client->rotate($data);
+        $this->assertEquals(
+            'rotate:a_90',
+            $client->input['action']
+        );
+    }
 
-	public function test_resize()
-	{
-		$client = new ImageProcessClient('', '');
-		$data = array (
-			'mode' => 1
-		);
-		$client->resize($data);
-		$this->assertEquals(
-			'resize:w_0,h_0,m_1',
-			$client->input['action']
-		);
-	}
+    public function test_resize()
+    {
+        $client = new ImageProcessClient('', '');
+        $data = array(
+            'mode' => 1
+        );
+        $client->resize($data);
+        $this->assertEquals(
+            'resize:w_0,h_0,m_1',
+            $client->input['action']
+        );
+    }
 
-	public function test_waterMark()
-	{
-		$client = new ImageProcessClient('', '');
-		$data = array (
-			'text' => '5rC05Y2w5paH5a2X'
-		);
-		$client->waterMark($data);
-		$this->assertEquals(
-			'watermark:d_150,p_0.25,t_5rC05Y2w5paH5a2X,c_',
-			$client->input['action']
-		);
-	}
+    public function test_waterMark()
+    {
+        $client = new ImageProcessClient('', '');
+        $data = array(
+            'text' => '5rC05Y2w5paH5a2X'
+        );
+        $client->waterMark($data);
+        $this->assertEquals(
+            'watermark:d_150,p_0.25,t_5rC05Y2w5paH5a2X,c_',
+            $client->input['action']
+        );
+    }
 
-	public function test_waterMarkImage()
-	{
-		$client = new ImageProcessClient('', '');
-		$data = array (
-			'url' => 'aHR0cHM6Ly9wZWszYS5xaW5nc3Rvci5jb20vaW1nLWRvYy1lZy9xaW5jbG91ZC5wbmc'
-		);
-		$client->waterMarkImage($data);
-		$this->assertEquals(
-			'watermark_image:l_0,t_0,p_0.25,u_aHR0cHM6Ly9w'.
-			'ZWszYS5xaW5nc3Rvci5jb20vaW1nLWRvYy1lZy9xaW5jbG91ZC5wbmc',
-			$client->input['action']
-		);
-	}
+    public function test_waterMarkImage()
+    {
+        $client = new ImageProcessClient('', '');
+        $data = array(
+            'url' => 'aHR0cHM6Ly9wZWszYS5xaW5nc3Rvci5jb20vaW1nLWRvYy1lZy9xaW5jbG91ZC5wbmc'
+        );
+        $client->waterMarkImage($data);
+        $this->assertEquals(
+            'watermark_image:l_0,t_0,p_0.25,u_aHR0cHM6Ly9w'.
+            'ZWszYS5xaW5nc3Rvci5jb20vaW1nLWRvYy1lZy9xaW5jbG91ZC5wbmc',
+            $client->input['action']
+        );
+    }
 
-	public function test_format()
-	{
-		$client = new ImageProcessClient('', '');
-		$data = array (
-			'type' => 'png'
-		);
-		$client->format($data);
-		$this->assertEquals(
-			'format:t_png',
-			$client->input['action']
-		);
-	}
+    public function test_format()
+    {
+        $client = new ImageProcessClient('', '');
+        $data = array(
+            'type' => 'png'
+        );
+        $client->format($data);
+        $this->assertEquals(
+            'format:t_png',
+            $client->input['action']
+        );
+    }
 
-	public function test_pipe() {
-		$client = new ImageProcessClient('', '');
+    public function test_pipe()
+    {
+        $client = new ImageProcessClient('', '');
 
-		$rotateParam = array (
-			'angle' => 90		
-		);
-		$cropParam = array (
-			'width' => 300,
-			'height' => 400,
-			'gravity' => 0
-		);
-		$resizeParam = array (
-			'width' => 500,
-			'height' => 500,
-			'mode' => 1
-		);
-		$formatParam = array (
-			'type' => 'png'		
-		);
-		$waterMarkParam = array (
-			'text' => '5rC05Y2w5paH5a2X'		
-		);
-		$waterMarkImageParam = array (
-			'url' => 'aHR0cHM6Ly9wZWszYS5xaW5nc3Rvci5jb20vaW1nLWRvYy1lZy9xaW5jbG91ZC5wbmc'
-		);
-		$client->rotate($rotateParam)
-			   ->crop($cropParam)
-			   ->resize($resizeParam)
-			   ->format($formatParam)
-			   ->waterMark($waterMarkParam)
-			   ->waterMarkImage($waterMarkImageParam)
-			   ->info();
-		$this->assertEquals(
-			'rotate:a_90|crop:w_300,h_400,g_0|resize:w_500,h_500,m_1|format:t_png|watermark:d_150,p_0.25,t_5rC05Y2w5paH5a2X,c_|watermark_image:l_0,t_0,p_0.25,u_aHR0cHM6Ly9wZWszYS5xaW5nc3Rvci5jb20vaW1nLWRvYy1lZy9xaW5jbG91ZC5wbmc|info',
-			$client->input['action']
-		);
-	}
+        $rotateParam = array(
+            'angle' => 90
+        );
+        $cropParam = array(
+            'width' => 300,
+            'height' => 400,
+            'gravity' => 0
+        );
+        $resizeParam = array(
+            'width' => 500,
+            'height' => 500,
+            'mode' => 1
+        );
+        $formatParam = array(
+            'type' => 'png'
+        );
+        $waterMarkParam = array(
+            'text' => '5rC05Y2w5paH5a2X'
+        );
+        $waterMarkImageParam = array(
+            'url' => 'aHR0cHM6Ly9wZWszYS5xaW5nc3Rvci5jb20vaW1nLWRvYy1lZy9xaW5jbG91ZC5wbmc'
+        );
+        $client->rotate($rotateParam)
+               ->crop($cropParam)
+               ->resize($resizeParam)
+               ->format($formatParam)
+               ->waterMark($waterMarkParam)
+               ->waterMarkImage($waterMarkImageParam)
+               ->info();
+        $this->assertEquals(
+            'rotate:a_90|crop:w_300,h_400,g_0|resize:w_500,h_500,m_1|format:t_png|watermark:d_150,p_0.25,t_5rC05Y2w5paH5a2X,c_|watermark_image:l_0,t_0,p_0.25,u_aHR0cHM6Ly9wZWszYS5xaW5nc3Rvci5jb20vaW1nLWRvYy1lZy9xaW5jbG91ZC5wbmc|info',
+            $client->input['action']
+        );
+    }
 }
